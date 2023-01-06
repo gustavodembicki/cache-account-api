b6f759b734f2725b9042e22db1d64986597a3cc5
Gustavo Felipe Dembicki <gustavodembicki@hotmail.com>
2023-01-05 23:08:44 -0300

    Creating cache control helper

---
+++ app/Helper/CacheHelper.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Helper;
+
+use Illuminate\Support\Facades\Cache;
+
+class CacheHelper
+{
+    /**
+     * Static methods responsable to recover a cache value;
+     * 
+     * @param string $cacheKey
+     * @return mixed
+     */
+    public static function get(string $cacheKey)
+    {
+        if (self::cacheExists($cacheKey)) {
+            return Cache::get($cacheKey);
+        }
+
+        return false;
+    }
+
+    /**
+     * Static method responsable to add a cache
+     * 
+     * @param string $cacheKey
+     * @param mixed $cacheValue
+     * @return void
+     */
+    public static function put(string $cacheKey, $cacheValue): void
+    {
+        Cache::put($cacheKey, $cacheValue);
+    }
+
+    /**
+     * Static method responsable to delete a cache
+     * 
+     * @param string $cacheKey
+     * @return void
+     */
+    public static function delete(string $cacheKey): void
+    {
+        Cache::forget($cacheKey);
+    }
+
+    /**
+     * Static method responsable to verify if cache exists
+     * 
+     * @param string $cacheKey
+     * @return bool
+     */
+    public static function cacheExists(string $cacheKey): bool
+    {
+        return Cache::has($cacheKey);
+    }
+}
