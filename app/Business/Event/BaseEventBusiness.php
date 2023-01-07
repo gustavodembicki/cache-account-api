ca490401cf97de82af7aeae28f9b500b07b66ee4
Gustavo Felipe Dembicki <gustavodembicki@hotmail.com>
2023-01-06 22:53:07 -0300

    Adding deposit and withdraw logic in /event route

--- app/Business/Event/BaseEventBusiness.php
+++ app/Business/Event/BaseEventBusiness.php
@@ -2,7 +2,8 @@
 
 namespace App\Business\Event;
 
-use Exception;
+use App\Helper\CacheHelper;
+use App\Helper\ResponseHelper;
 use Illuminate\Http\Response;
 
 abstract class BaseEventBusiness
@@ -19,25 +20,61 @@ abstract class BaseEventBusiness
      */
     public function typeRead()
     {
-        print_r($this->data);
+        $typeExists = false;
+        $actualType = "";
 
-        $typeExists = $this->typesAccepted[$this->data["type"]] ?? false;
-
-        if (!$typeExists) {
-            throw new Exception("Type informed doesn't exists", Response::HTTP_UNPROCESSABLE_ENTITY);
+        foreach ($this->typesAccepted as $typeAccepted) {
+            if ($typeAccepted == $this->data["type"]) {
+                $typeExists = true;
+                $actualType = $this->data["type"];
+                break;
+            }
         }
 
-        return $this->typesAccepted[$this->data["type"]]();
+        if (!$typeExists) {
+            return ResponseHelper::return(Response::HTTP_UNPROCESSABLE_ENTITY, ["message" => "Type informed doesnt exists"]);
+        }
+
+        return $this->$actualType();
     }
 
     private function deposit()
     {
+        if (CacheHelper::cacheExists("account_{$this->data["destination"]}")) {
+            $accountCache = CacheHelper::get("account_{$this->data["destination"]}");
+            $this->data["amount"] += $accountCache["amount"];
+        }
+        
+        CacheHelper::put("account_{$this->data["destination"]}", $this->data);
 
+        return ResponseHelper::return(gettype($accountCache) != "undefined" ? 200 : 201, [
+            "destination" => [
+                "id" => $this->data["destination"],
+                "balance" => $this->data["amount"]
+            ],
+            "cache_test" => CacheHelper::get("account_{$this->data["destination"]}") //Temporary 
+        ]);
     }
 
     private function withdraw()
     {
+        $accountCacheName = "account_{$this->data["origin"]}";
 
+        if (!CacheHelper::cacheExists($accountCacheName)) {
+            return ResponseHelper::return(Response::HTTP_NOT_FOUND);
+        }
+
+        $accountCache = CacheHelper::get($accountCacheName);
+        $accountCache["amount"] -= $this->data["amount"];
+
+        CacheHelper::put($accountCacheName, $accountCache);
+
+        return ResponseHelper::return(Response::HTTP_CREATED, [
+            "origin" => [
+                "id" => $this->data["origin"],
+                "balance" => $accountCache["amount"]
+            ]
+        ]);
     }
 
     private function transfer()
